b070e6f6002002cec7635e4f511a2fa39ee1e4e0
Aseel Hamamreh <aseel.hamamreh@hotmail.com>
2021-03-31 01:44:42 +0300

    lab-12 done

--- js/app.js
+++ js/app.js
@@ -1,108 +1,120 @@
-'use strict';
+'use stricts';
 
 let clicksNumber=0;
 let votes = [];
 let views = [];
+let indexes =[];
+
 const names = ['bag','banana','bathroom','boots','breakfast','bubblegum','chair','cthulhu','dog-duck','dragon','pen','pet-sweep','scissors','shark','sweep','tauntaun','unicorn','usb','water-can','wine-glass',];
-function Item(name) {
-  this.name = name;
-  this.path = `./assets/${name}.jpg`;
-  this.votes = 0;
-  this.views = 0;
+
+function Item (name){
+  this.name= name;
+  this.path=`./assets/${name}.jpg`;
+  this.votes=0;
+  this.views=0;
   Item.all.push(this);
 }
-Item.all = [];
+Item.all=[];
 
-for(let i =0;i<names.length;i++){
+for (let i=0; i<names.length;i++){
   new Item(names[i]);
 }
 
-const leftImage = document.getElementById('left-image');
-const centerImage = document.getElementById('center-image');
-const rightImage = document.getElementById('right-image');
-const imagesSection = document.getElementById('images-section');
+// console.log(Item.all);
 
-
-function render(){
-  if (clicksNumber <=25){
-    let leftIndex=randomNumber(0,names.length-1);
-    leftImage.src = Item.all[leftIndex].path;
-    leftImage.alt = Item.all[leftIndex].name;
-    leftImage.title = Item.all[leftIndex].name;
-    Item.all[leftIndex].views++;
-
-    let centerIndex=randomNumber(0,names.length-1);
-    if (centerIndex === leftIndex || centerIndex === leftIndex){
-      centerIndex=randomNumber(0,names.length-1);
-    }
-    centerImage.src = Item.all[centerIndex].path;
-    centerImage.alt = Item.all[centerIndex].name;
-    centerImage.title = Item.all[centerIndex].name;
-    Item.all[centerIndex].views++;
-
-    let rightIndex= randomNumber(0,names.length-1);
-    if (rightIndex === centerIndex || rightIndex === leftIndex){
-      rightIndex= randomNumber(0,names.length-1);
-    }
-    rightImage.src = Item.all[rightIndex].path;
-    rightImage.alt = Item.all[rightIndex].name;
-    rightImage.title = Item.all[rightIndex].name;
-    Item.all[rightIndex].views++;
-  }
-}
+let leftImage=document.getElementById('left-image');
+let centerImage=document.getElementById('center-image');
+let rightImage=document.getElementById('right-image');
 
 function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-imagesSection.addEventListener('click',handelClick);
-function handelClick(event){
-  event.preventDefault();
-  if(event.target.id !== 'images-section'){
-    for(let i=0; i<Item.all.length; i++){
-      if(Item.all[i].name === event.target.title && clicksNumber <= 25){
-        Item.all[i].votes++;
-        clicksNumber=clicksNumber+1;
-        if (clicksNumber ===25){
-          resultButton();
-          reset();
-        }
+function render(){
+  let leftIndex=randomNumber(0,names.length-1);
+  let centerIndex=randomNumber(0,names.length-1);
+  let rightIndex=randomNumber(0,names.length-1);
 
+  if (leftIndex !== centerIndex && leftIndex !== rightIndex && centerIndex !== rightIndex){
+
+    if (indexes.includes(leftIndex) || indexes.includes(centerIndex) || indexes.includes(rightIndex)){
+      render();
+    }
+    else{
+      indexes =[];
+      indexes.push(leftIndex);
+      indexes.push(centerIndex);
+      indexes.push(rightIndex);
+      console.log(indexes);
+
+      if (clicksNumber <= 25){
+        leftImage.src=Item.all[leftIndex].path;
+        leftImage.alt=Item.all[leftIndex].name;
+        leftImage.title=Item.all[leftIndex].name;
+        Item.all[leftIndex].views++;
+
+        centerImage.src=Item.all[centerIndex].path;
+        centerImage.alt=Item.all[centerIndex].name;
+        centerImage.title=Item.all[centerIndex].name;
+        Item.all[centerIndex].views++;
+
+
+        rightImage.src=Item.all[rightIndex].path;
+        rightImage.alt=Item.all[rightIndex].name;
+        rightImage.title=Item.all[rightIndex].name;
+        Item.all[rightIndex].views++;
       }
     }
   }
-  render();
+  else{
+    render();
+  }
 }
-
-console.log(Item.all);
 
 render();
 
 
-const result =document.getElementById('result');
 
 
-function resultButton(){
+let section=document.getElementById('images-section');
+
+section =document.addEventListener('click',afterClicking);
+function afterClicking(event){
+//   console.log(event.target.id);
+  if (event.target.id !== 'images-section'){
+    for (let i=0;i<Item.all.length;i++){
+      if(Item.all[i].name === event.target.title && clicksNumber<=25){
+        Item.all[i].votes++;
+        clicksNumber=clicksNumber+1;
+        if (clicksNumber===25) {
+          showResult();
+          reset();
+        }
+      }
+    } render();
+  }
+}
+
+
+const result = document.getElementById('result');
+
+function showResult() {
   const button = document.createElement('button');
   result.appendChild(button);
-  button.textContent='VIEW RESULTS';
-  button.addEventListener ('click', function() {
-    const ul = document.createElement('ul');
-    result.appendChild(ul);
-    for(let i=0; i<names.length; i++){
+  button.textContent='SHOW RESULT';
+  button.addEventListener('click', buttonSittings);
+  function buttonSittings(){
+    const ulEl = document.createElement('ul');
+    result.appendChild(ulEl);
+    for (let i=0; i<names.length; i++){
       votes.push(Item.all[i].votes);
       views.push(Item.all[i].views);
-
-      // const li = document.createElement('li');
-      // ul.appendChild(li);
-      // li.textContent= `${Item.all[i].name} had ${Item.all[i].votes} votes, and was seen ${Item.all[i].views} times.`;
+      const liEl = document.createElement('li');
+      ulEl.appendChild(liEl);
+      liEl.textContent= `${Item.all[i].name} had ${Item.all[i].votes} votes, and was seen ${Item.all[i].views} times.`;
     }
-    console.log(votes);
-    console.log(views);
     chartDisplay();
-
-
-  });
+  }
 }
 
 function reset(){
@@ -114,10 +126,8 @@ function reset(){
   });
 }
 
-
 function chartDisplay() {
   let ctx = document.getElementById('myChart').getContext('2d');
-  // eslint-disable-next-line no-undef
   let myChart = new Chart(ctx, {
     type: 'bar',
     data: {
@@ -133,10 +143,7 @@ function chartDisplay() {
         data: views,
         backgroundColor: '#EFC9AF',
         borderColor: 'black',
-
       }]
     },
-
   });
-
 }
